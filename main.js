eb2411d9ab1dc9e2c4941b2618e68eef0dc9c5cc
yourseulah <imseulahlee@gmail.com>
2022-12-31 15:59:52 -0800

    JS: Arrow Up

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function scrollIntoViews(selector) {
   scrollTo.scrollIntoView({behavior: "smooth"});
 }
 
-//Make home slowly fade to transparent as the window scrolls down
+//Make home content slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -55,6 +55,20 @@ document.addEventListener('scroll', () => {
   //console.log(1 - window.scrollY / homeHeight);
 })
 
+//Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+//Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoViews('#home');
+})
 
 /*
 
