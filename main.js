b34d7caeb4ff37df137e14a76121a8fea11d0492
yourseulah <imseulahlee@gmail.com>
2022-12-31 21:13:58 -0800

    JS : remove active from nav

--- main.js
+++ main.js
@@ -80,6 +80,13 @@ workBtnContainer.addEventListener('click', (e) => {
     return;
   }
   // console.log(filter);
+
+  //Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected')
+  active.classList.remove('selected');
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode; //condition ? if yes : if no
+  target.classList.add('selected')
+
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
     projects.forEach((project) => {
@@ -110,39 +117,3 @@ workBtnContainer.addEventListener('click', (e) => {
   
 });
 
-
-/*
-
-// Projects
-const workBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e) => {
-  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter == null) {
-    return;
-  }
-
-  // Remove selection from the previous item and select the new one
-  const active = document.querySelector('.category__btn.selected');
-  if (active != null) {
-    active.classList.remove('selected');
-  }
-  e.target.classList.add('selected');
-
-  projectContainer.classList.add('anim-out');
-  setTimeout(() => {
-    projects.forEach((project) => {
-      console.log(project.dataset.type);
-      if (filter === '*' || filter === project.dataset.type) {
-        project.classList.remove('invisible');
-      } else {
-        project.classList.add('invisible');
-      }
-    });
-    projectContainer.classList.remove('anim-out');
-  }, 300);
-});
-
-
-*/
