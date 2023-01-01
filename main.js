84129f32bc52d065b86b2008041cf20566ac2422
yourseulah <imseulahlee@gmail.com>
2022-12-31 20:40:02 -0800

    JS: project filtering

--- main.js
+++ main.js
@@ -26,8 +26,7 @@ document.addEventListener('scroll', () => {
 //Handle scroll when clicking the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
+  const link = event.target.dataset.link;
   if(link == null) {
     return;
   }
@@ -69,6 +68,48 @@ document.addEventListener('scroll', () => {
 arrowUp.addEventListener('click', () => {
   scrollIntoViews('#home');
 })
+
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  // console.log(filter);
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter ==='*' || filter === project.dataset.type) {
+        project.classList.remove('invisible')
+      } else {
+        project.classList.add('invisible')
+      } 
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+
+
+  /* 2 other ways (same as forEach) :
+  console.log(`------------`);
+  for (let project of projects) {
+    console.log(project);
+  }
+
+  console.log(`------------`);
+  let project;
+  for(let i=0; i < projects.length; i++) {
+    project = projects[i];
+    console.log(project);
+  }
+  */
+  
+});
+
 
 /*
 
